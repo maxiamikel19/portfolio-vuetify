030bc6677b8681d3c008abe01fe5960c17ca6e8a
maxiamikel19 <maxloversist19@gmail.com>
2024-06-29 04:22:23 -0300

    Instalar dependencias de vuetify e criar v-toolbar e v-navigaton

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app grey lighten-4>
+    <v-navbar />
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import VNavbar from '@/components/VNavbar.vue'
 
-nav {
-  padding: 30px;
-}
+export default {
+  name: 'App',
+  components: { VNavbar },
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  data: () => ({
+    //
+  }),
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ src/components/VNavbar.vue
@@ -0,0 +1,48 @@
+<template>
+    <nav>
+        <v-toolbar
+            app
+            flat
+            dark
+            prominent
+            color="indigo-accent-2"
+        >
+        <v-app-bar-nav-icon 
+            variant="text" 
+            @click.stop="drawer = !drawer"
+            color="white"
+        ></v-app-bar-nav-icon>
+
+        <v-toolbar-title
+            class="text-uppercase text-white"
+        >
+            <span class=" font-white">Max</span>
+            <span>Home</span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn flat color="white" >
+            <span>Logout</span>
+            <v-icon end>mdi-export</v-icon>
+        </v-btn>
+        </v-toolbar>
+
+        <v-navigation-drawer app class="indigo" v-model="drawer" color="indigo-accent-2">
+            
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'VNavbar',
+        data(){
+            return{
+                drawer: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
